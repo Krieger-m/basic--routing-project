0bf4fa5bbd639641e253dc06050eb18f6e830b7b
Mat <matze.krieger@googlemail.com>
2025-10-20 16:42:05 +0200

    added more comments & removed unused stuff

--- routing-test-frontend/src/app/page.tsx
+++ routing-test-frontend/src/app/page.tsx
@@ -1,21 +1,25 @@
 import styles from "./page.module.css";
 
+
+
 async function homePageLoader(){
 
   const path = '/api/home-page';
   const BASE_URL = 'http://localhost:1337';
-  const url = new URL(path,BASE_URL); 
+  const url = new URL(path,BASE_URL);   // builds url from BASE_URL and path
 
   const response = await fetch(url.href);
-  const data = await response.json();
+  const data = await response.json();   // getting data via default-fetch
 
   // console.log(data);
-  return { ...data.data };
+  return { ...data.data };    // returns destructured data
 }
+
+
 
 export default async function HomeRoute() {
 
-  const home = await homePageLoader();
+  const home = await homePageLoader();  // receive data from homepageloader
 
   // console.log(home);
 
@@ -24,5 +28,5 @@ export default async function HomeRoute() {
       <h1>{home.title}</h1>
       <p>{home.description}</p>
     </div>
-  );
+  );    // standard stuff
 }
