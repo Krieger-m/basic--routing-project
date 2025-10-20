99810ded42f9ddbfc7cc67c000ddf1f4af12fb7b
Mat <matze.krieger@googlemail.com>
2025-10-20 16:32:07 +0200

    added more comments

--- routing-test-frontend/src/app/[slug]/page.tsx
+++ routing-test-frontend/src/app/[slug]/page.tsx
@@ -11,27 +11,27 @@ async function dynamicPageLoader(slug: string){
 
   const path = '/api/pages';
   const BASE_URL = 'http://localhost:1337';
-  const url = new URL(path,BASE_URL); 
+  const url = new URL(path,BASE_URL);   // creates url
 
 
   url.search = new URLSearchParams({
     'filters[slug][$eq]': slug,
-  }).toString();
+  }).toString();    // filters results by slug 
 
   const response = await fetch(url.href);
   const data = await response.json();
 
   // console.log(data);
-  return { ...data.data[0]};    // ...data.data[0] because of the received datastructure. can be seen and tested using the console.logs above here and below in DynamicPage()
+  return { ...data.data[0]};    // ...data.data[0] because of the received datastructure. data needs to be destructured like seen here to be handled efficiently. can be seen and tested using the console.logs above here and below in DynamicPage()
 }
 
 export default async function DynamicPage({
   params,
 }: DynamicPageProps) {
 
-  const slug = (await params).slug;
+  const slug = (await params).slug; // creates slug from url-param
 
-  const data = await dynamicPageLoader(slug);
+  const data = await dynamicPageLoader(slug);   // awaits data from pageloader function
 
     // console.log(data);
     // console.log(slug);
